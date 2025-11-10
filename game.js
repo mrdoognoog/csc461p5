e920ceac17031a671c5cad8ae33fef422d1fb1fe
wldegnan <wldegnan@ncsu.edu>
2025-11-10 16:12:02 -0500

    import skeleton

---
+++ game.js
@@ -0,0 +1,93 @@
+/*battlezone */
+
+/*global constants and variables */
+
+/* webgl and geometry data */
+var gl = null; // the all powerful gl object. It's all here folks!
+
+// does stuff when keys are pressed
+function handleKeyDown(event) {
+    
+    const modelEnum = {TRIANGLES: "triangles", ELLIPSOID: "ellipsoid"}; // enumerated model type
+    const dirEnum = {NEGATIVE: -1, POSITIVE: 1}; // enumerated rotation direction
+    
+    function highlightModel(modelType,whichModel) {
+        if (handleKeyDown.modelOn != null)
+            handleKeyDown.modelOn.on = false;
+        handleKeyDown.whichOn = whichModel;
+        if (modelType == modelEnum.TRIANGLES)
+            handleKeyDown.modelOn = inputTriangles[whichModel]; 
+        else
+            handleKeyDown.modelOn = inputEllipsoids[whichModel]; 
+        handleKeyDown.modelOn.on = true; 
+    } // end highlight model
+    
+    function translateModel(offset) {
+        if (handleKeyDown.modelOn != null)
+            vec3.add(handleKeyDown.modelOn.translation,handleKeyDown.modelOn.translation,offset);
+    } // end translate model
+
+    function rotateModel(axis,direction) {
+        if (handleKeyDown.modelOn != null) {
+            var newRotation = mat4.create();
+
+            mat4.fromRotation(newRotation,direction*rotateTheta,axis); // get a rotation matrix around passed axis
+            vec3.transformMat4(handleKeyDown.modelOn.xAxis,handleKeyDown.modelOn.xAxis,newRotation); // rotate model x axis tip
+            vec3.transformMat4(handleKeyDown.modelOn.yAxis,handleKeyDown.modelOn.yAxis,newRotation); // rotate model y axis tip
+        } // end if there is a highlighted model
+    } // end rotate model
+    
+    // set up needed view params
+    var lookAt = vec3.create(), viewRight = vec3.create(), temp = vec3.create(); // lookat, right & temp vectors
+    lookAt = vec3.normalize(lookAt,vec3.subtract(temp,Center,Eye)); // get lookat vector
+    viewRight = vec3.normalize(viewRight,vec3.cross(temp,lookAt,Up)); // get view right vector
+    
+    // highlight static variables
+    handleKeyDown.whichOn = handleKeyDown.whichOn == undefined ? -1 : handleKeyDown.whichOn; // nothing selected initially
+    handleKeyDown.modelOn = handleKeyDown.modelOn == undefined ? null : handleKeyDown.modelOn; // nothing selected initially
+
+    switch (event.code) {
+        
+        // model selection
+        case "Space": 
+            console.log("hi");
+            break;
+    } // end switch
+} // end handleKeyDown
+
+
+// set up the webGL environment
+function setupWebGL() {
+    
+    // Set up keys
+    document.onkeydown = handleKeyDown; // call this when key pressed
+
+
+    var imageCanvas = document.getElementById("myImageCanvas"); // create a 2d canvas
+      var cw = imageCanvas.width, ch = imageCanvas.height; 
+      imageContext = imageCanvas.getContext("2d"); 
+
+     
+    // Get the canvas and context
+    var canvas = document.getElementById("myWebGLCanvas"); // create a js canvas
+    gl = canvas.getContext("webgl"); // get a webgl object from it
+    
+    try {
+      if (gl == null) {
+        throw "unable to create gl context -- is your browser gl ready?";
+      } else {
+        //gl.clearColor(0.0, 0.0, 0.0, 1.0); // use black when we clear the frame buffer
+        gl.clearDepth(1.0); // use max when we clear the depth buffer
+        gl.enable(gl.DEPTH_TEST); // use hidden surface removal (with zbuffering)
+      }
+    } // end try
+    
+    catch(e) {
+      console.log(e);
+    } // end catch
+ 
+} // end setupWebGL
+
+function main(){
+    setupWebGL();
+}
